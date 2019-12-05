b9b9ffc2c67a8c75226df12bf7635e780522cb0b
Iago Vitoriano <iago.bezerra@restoque.com.br>
2019-12-05 14:29:56 -0300

    fix identation

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 var a;
 function teste1() {
-  var a = 3;
+    var a = 3;
 }
 teste1();
 console.log(a === 3);
@@ -12,25 +12,25 @@ console.log(a === 3);
  * ***********************************/
 
 function teste2() {
-  var arr = [1, 2, 3];
-  for (var i = 0; i < arr.length; i++) {
-    setTimeout(function() {
-      console.log(`Index: ${i};\nElement: ${arr[i]}`);
-    }, 0);
+    var arr = [1, 2, 3];
+    for (var i = 0; i < arr.length; i++) {
+        setTimeout(function() {
+            console.log(`Index: ${i};\nElement: ${arr[i]}`);
+        }, 0);
+    }
   }
-}
-teste2();
+  teste2();
 /* ***********************************
  * (2) Qual o valor escrito no console e por quê?
  * ***********************************/
 
 function inc(i) {
-  i++;
+    i++;
 }
 function teste3() {
-  var i = 0;
-  inc(i);
-  console.log(i);
+    var i = 0;
+    inc(i);
+    console.log(i);
 }
 teste3();
 /* ***********************************
@@ -38,10 +38,10 @@ teste3();
  * ***********************************/
 
 function teste4() {
-  var value = 10;
-  return function() {
-    return value;
-  };
+    var value = 10;
+    return ( function () {
+        return value;
+    })
 }
 func = teste4();
 console.log(func());
@@ -51,21 +51,21 @@ console.log(func());
 
 // A função shuffle recebe como entrada uma array e a embaralha.
 function shuffle(a) {
-  // atribui a variável n a quantidades de elementos da array a.
-  n = a.length;
-  // Percorre a array a da posição 0 até a ultima posição.
-  for (var i = 0; i < n; i++) {
-    //Sorteia uma posição aleatória entre i e n-1 e atribui para a variável swap
-    swap = i + Math.floor(Math.random() * (n - i - 1));
-    // Troca os valores das posições i e swap da array a.
-    var aux = a[i];
-    a[i] = a[swap];
-    a[swap] = aux;
-  }
+    // atribui a variável n a quantidades de elementos da array a.
+    n = a.length;
+    // Percorre a array a da posição 0 até a ultima posição.
+    for (var i = 0; i < n; i++) {
+        //Sorteia uma posição aleatória entre i e n-1 e atribui para a variável swap
+        swap = i + Math.floor(Math.random() * (n - i - 1));
+        // Troca os valores das posições i e swap da array a. 
+        var aux = a[i];
+        a[i] = a[swap];
+        a[swap] = aux;
+    }
 }
 var array = [5, 3, 1, 4, 2];
 if (shuffle(array)) console.log(array);
 /* *********************************
- * (5) Existem potenciais armadilhas (pitfalls) na função acima?
+ * (5) Existem potenciais armadilhas (pitfalls) na função acima? 
  * Se SIM qual é ou quais são? Comente a resposta.
  * *********************************/
